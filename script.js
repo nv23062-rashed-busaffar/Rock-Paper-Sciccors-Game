11b1c1870174b552bf5bf1cc2d202e648fe30de8
nv23062-rashed-busaffar <NV23062@nvtc.edu.bh>
2025-04-14 08:06:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
+let humanScore = 0;
+let computerScore = 0;
 
+function getComputerChoice() {
+    const randomNum = Math.random();
+    if (randomNum < 0.33) {
+        return "rock";
+    } else if (randomNum < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function playRound(humanChoice) {
+    const computerChoice = getComputerChoice();
+    let resultMessage = '';
+
+    if (humanChoice === computerChoice) {
+        resultMessage = "It's a tie!";
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        resultMessage = `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`;
+        humanScore++;
+    } else {
+        resultMessage = `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.`;
+        computerScore++;
+    }
+
+    document.getElementById('result').innerText = resultMessage;
+    document.getElementById('score').innerText = `Score - You: ${humanScore}, Computer: ${computerScore}`;
+
+    if (humanScore === 5) {
+        alert("Congratulations! You are the overall winner!");
+        resetGame();
+    } else if (computerScore === 5) {
+        alert("The computer is the overall winner!");
+        resetGame();
+    }
+}
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    document.getElementById('result').innerText = '';
+    document.getElementById('score').innerText = '';
+}
+
+document.getElementById('rock').addEventListener('click', () => playRound('rock'));
+document.getElementById('paper').addEventListener('click', () => playRound('paper'));
+document.getElementById('scissors').addEventListener('click', () => playRound('scissors'));
